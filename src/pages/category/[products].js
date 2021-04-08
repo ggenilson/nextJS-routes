cd5d2bfb54fa36ba9fe70e5b40b63973d785dc29
ggenilson <genilsonaraujo20002002@gmail.com>
2021-04-08 16:20:06 +0100

    updates

--- src/pages/category/[products].js
+++ src/pages/category/[products].js
@@ -4,9 +4,10 @@ export default function Products() {
   const router = useRouter();
 
   console.log('Here: ', router);
+
   return (
     <div>
-      <h1>Produto</h1>
+      <h1>Produto {router.query.products}</h1>
     </div>
   );
 }
